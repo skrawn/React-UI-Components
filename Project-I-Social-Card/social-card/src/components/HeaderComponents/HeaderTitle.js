8ed1c094a3c5c545d196a17d149018d0634f4b2d
Sean Donohue <sdonohue@developertown.com>
2019-01-18 13:29:02 -0700

    Created the header container and components within it. Still needs styling.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div>
+            <h2>Lambda School</h2>
+            <p>@LambaSchool - 26 jan</p>
+        </div>
+    );
+};
+
+export default HeaderTitle;
