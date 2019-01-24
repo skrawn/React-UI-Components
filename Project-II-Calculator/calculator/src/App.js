608d3e0edd085576a848a7b293c0a6dd7faed502
Sean Donohue <sdonohue@developertown.com>
2019-01-24 15:27:02 -0700

    Have the base grid set up but need to fix the flexbox properties so all buttons are evenly distributed.

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import './App.css';
 
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
+
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="calculator-container">
+      <CalculatorDisplay />
+      <div className="button-row">
+        <ActionButton text={'clear'} buttonStyle="zero-button" />
+      </div>
+      <div className="button-row">
+        <NumberButton text={7} buttonStyle="button-class" />
+        <NumberButton text={8} buttonStyle="button-class" />
+        <NumberButton text={9} buttonStyle="button-class" />
+        <ActionButton text={'X'} buttonStyle="action-button" />
+      </div>
+      <div className="button-row">
+        <NumberButton text={4} buttonStyle="button-class" />
+        <NumberButton text={5} buttonStyle="button-class" />
+        <NumberButton text={6} buttonStyle="button-class" />
+        <ActionButton text={'-'} buttonStyle="action-button" />
+      </div>
+      <div className="button-row">
+        <NumberButton text={1} buttonStyle="button-class" />
+        <NumberButton text={2} buttonStyle="button-class" />
+        <NumberButton text={3} buttonStyle="button-class" />
+        <ActionButton text={'+'} buttonStyle="action-button" />
+      </div>
+      <div className="button-row">
+        <ActionButton text={0} buttonStyle="zero-button" />
+        <ActionButton text={'='} buttonStyle="action-button" />
+      </div>
     </div>
   );
 };
